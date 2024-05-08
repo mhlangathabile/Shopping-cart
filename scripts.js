070fc8779334428b21ca9e08e1b852863d636a4c
mhlangathabile <mhlangathabile27@gmail.com>
2024-05-08 23:49:31 +0200

    Javascript HTML Append

--- scripts.js
+++ scripts.js
@@ -21,9 +21,29 @@ function addToCartButton(event) {
   let imageSrc = cartItem.getElementsByClassName("images")[0].src;
   let cartPrice = cartItem.querySelector(".prices").innerText;
   console.log(imageSrc, cartPrice);
+  addToCartItem(imageSrc, cartPrice);
 }
 
 function addToCartItem(imageSrc, cartPrice) {
-  let cartRow = document.createElement("div");
-  let;
+  let cartContainer = document.createElement("div");
+  let cartItems = document.getElementsByClassName("cart-container")[0];
+  let cartContents = `
+   <div class="cart-container">
+      <div class="row cart">
+        <div class="col-3" id="cartImage"><h2 class="heading">ITEM</h2></div>
+        <div class="col-3" id="cartPrice"><h2 class="heading">PRICE</h2></div>
+        <div class="col-3" id="cartQuantity">
+          <h2 class="heading">QUANTITY</h2>
+        </div>
+        <div class="col-3" id="cartDelete">
+          <h2 class="heading">DELETE</h2>
+          <button class="btn btn-dark cartDelete" id="cartDelete" type="button">
+            X
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+  cartContainer.innerHTML = cartContents;
+  cartItems.append(cartContainer);
 }
