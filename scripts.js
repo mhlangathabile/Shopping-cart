30a43d0e26e5a849be69355e6c783840f1d04800
mhlangathabile <mhlangathabile27@gmail.com>
2024-05-08 12:59:15 +0200

    Delete Cart Element

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
-function addToCart {
-    let imageElement = document.querySelector(#images);
-    let imageUrl = document.imageElement.
+let deleteCartButton = document.querySelectorAll(".cartDelete");
+for (let i = 0; i < deleteCartButton.length; i++) {
+  let button = deleteCartButton[i];
+  button.addEventListener("click", function (event) {
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+  });
 }
